a2be05d5db583ffa516fbdb6e679f0f49442a952
Khrapov Denys <khrapov.denys@gmail.com>
2022-05-13 13:11:12 +0300

    wp-team

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Custom scripts
+$(document).ready(function () {
+	$(".nav-burger").click(function (event) {
+		$(".nav-burger, .nav-menu").toggleClass("active");
+		$("body").toggleClass("locked");
+	});
+	$(".header-body__cross").click(function (event) {
+		$(".header").removeClass("active");
+	});
+});
+
